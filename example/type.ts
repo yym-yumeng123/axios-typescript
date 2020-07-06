7fc18965e3b0daaccd33229d04a9cf07c434ac43
yym-yumeng123 <18026493873@163.com>
2020-07-06 23:14:32 +0800

    docs: 普通类型 & 数组 & 元组

---
+++ example/type.ts
@@ -0,0 +1,16 @@
+const isDone: boolean = false
+
+const decLiteral: number = 20
+const hexLiteral: number = 0x14
+const bingryLiteral: number = 0b1010
+const octalLiteral: number = 0o24
+
+const str: string = 'bob' 
+
+// 数组
+const list: number[] = [1,2,3]
+const list1: Array<number> = [1, 3, 5]
+
+// 元组
+const x: [string, number] = ['yym', 12]
+
