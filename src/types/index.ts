a511ae647604a8d43f1501b8836019c50452a4d6
yym-yumeng123 <18026493873@163.com>
2020-10-22 23:53:43 +0800

    feat(src): 入口
    添加入口文件 & 定义类型

---
+++ src/types/index.ts
@@ -0,0 +1,22 @@
+export type Method =
+  | 'get'
+  | 'GET'
+  | 'delete'
+  | 'DELETE'
+  | 'head'
+  | 'HEAD'
+  | 'options'
+  | 'OPTIONS'
+  | 'post'
+  | 'POST'
+  | 'put'
+  | 'PUT'
+  | 'patch'
+  | 'patch'
+
+export interface AxiosRequestConfig {
+  url: string
+  method?: Method
+  data?: any
+  params?: any
+}
