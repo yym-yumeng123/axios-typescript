f289598f08f281ef220395734940d066d5076bd1
yym-yumeng123 <18026493873@163.com>
2020-10-24 14:01:31 +0800

    打通 demo 测试

--- demo/server.js
+++ demo/server.js
@@ -5,17 +5,18 @@ const webpackDevMiddleware = require('webpack-dev-middleware')
 const webpackHotMiddleware = require('webpack-hot-middleware')
 const WebpackConfig = require('./webpack.config')
 
-
 const app = express()
 const compiler = webpack(WebpackConfig)
 
-app.use(webpackDevMiddleware(compiler, {
-  publicPath: '/__build__/',
-  stats: {
-    colors: true,
-    chunks: false
-  }
-}))
+app.use(
+  webpackDevMiddleware(compiler, {
+    publicPath: '/__build__/',
+    stats: {
+      colors: true,
+      chunks: false
+    }
+  })
+)
 
 app.use(webpackHotMiddleware(compiler))
 app.use(express.static(__dirname))
@@ -27,7 +28,7 @@ const router = express.Router()
 
 router.get('/simple/get', function(req, res) {
   res.json({
-    msg: `Hello world`
+    msg: `hello world`
   })
 })
 
@@ -35,6 +36,7 @@ app.use(router)
 
 const port = process.env.PORT || 8080
 module.exports = app.listen(port, () => {
-  console.log(`Server listening on http:// localhost:${port}`)
+  console.log(`Server listening on http://localhost:${port}, Ctrl+C to stop`)
 })
+
 
